acba9720c4be138aa478982775bfd9507ca9effc
Covenant Junior <covenant@gmail.com>
2023-06-14 15:37:01 +0100

    First Compilation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [text, setText] = useState('');
+  const [output, setOutput] = useState('');
+
+  const handlePlay = () => {
+    const synthesis = window.speechSynthesis;
+    const utterance = new SpeechSynthesisUtterance(text);
+    synthesis.speak(utterance);
+  };
+
+  const handleSpeak = () => {
+    const recognition = new window.webkitSpeechRecognition() || window.SpeechRecognition;
+    const recognitionInstance = new recognition();
+
+    recognitionInstance.onresult = (event) => {
+      const { transcript } = event.results[0][0];
+      setOutput(transcript);
+      setText(transcript);
+    };
+
+    recognitionInstance.onerror = (event) => {
+      setOutput('Error: ' + event.error);
+    };
+
+    recognitionInstance.start();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <div className="form-group mt-5">
+        <label htmlFor="title">Speech Recognition</label>
+        <textarea
+          id="text"
+          className="form-control"
+          rows="10"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        ></textarea>
+      </div>
+      <button id="play" className="btn btn-warning mr-2" onClick={handlePlay}>
+        Play
+      </button>
+      <button id="speak" className="btn btn-danger mr-2" onClick={handleSpeak}>
+        Speak
+      </button>
+      <div className="mt-3">
+        <p id="output">{output}</p>
+      </div>
     </div>
   );
 }
