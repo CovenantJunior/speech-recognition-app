28a61818cada41e80fc0303e8533e85252f3cae7
pablo-codes <112553944+pablo-codes@users.noreply.github.com>
2023-06-15 13:20:01 +0100

    text

--- src/App.js
+++ src/App.js
@@ -5,40 +5,55 @@ import './App.css'
 const App = () => {
 
   const { transcript, resetTranscript } = useSpeechRecognition();
+  const [text, setText] = useState('')
+  const [isListening, setListening] = useState(false)
 
   const handlePlay = () => {
     const synthesis = window.speechSynthesis;
-    const utterance = new SpeechSynthesisUtterance(transcript);
+    const utterance = new SpeechSynthesisUtterance(text);
     synthesis.speak(utterance);
   };
 
   const handleSpeak = () => {
     resetTranscript()
+    setListening(true)
     SpeechRecognition.startListening({
       continuous: true,
     });
+
   };
   const handleStop = () => {
     SpeechRecognition.stopListening();
+    setListening(false)
+    setText(transcript)
   };
   return (
     <div className="container mt-5">
       <div className="form-group mt-5">
         <label htmlFor="title">Speech Recognition</label>
-        <textarea
+        {isListening ? <textarea
           id="text"
           className="form-control"
           rows="10"
           value={transcript}
-        ></textarea>
+        ></textarea> : <textarea
+          id="text"
+          className="form-control"
+          rows="10"
+          value={text}
+          onChange={(event) => {
+            setText(event.target.value)
+          }}
+        ></textarea>}
+
       </div>
-      <button id="play" className="btn btn-warning mr-2" onClick={handlePlay}>
+      <button id="play" className="btn btn-success mr-2" onClick={handlePlay}>
         Play
       </button>
-      <button id='stop' onClick={handleStop} className='btn btn-success mr-2'>
+      <button id='stop' onClick={handleStop} className='btn btn-danger mr-2'>
         Stop
       </button>
-      <button id="speak" className="btn btn-danger mr-2" onClick={handleSpeak}>
+      <button id="speak" className="btn btn-warning  mr-2" onClick={handleSpeak}>
         Speak
       </button>
       {/* <div className="mt-3">
